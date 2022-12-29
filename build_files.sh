2b336ebe87984c1ba34dd7427ab0a23ba63b49e3
romelias <120766185+romelias@users.noreply.github.com>
2022-12-29 13:44:46 +0530

    3committ

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,7 @@
 # build_files.sh
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+#python3.9 manage.py makemigrations 
+#python3.9 manage.py migrate 
