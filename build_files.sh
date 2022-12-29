8c4f65eba869d887f3c6ce6159775b582443762d
romelias <120766185+romelias@users.noreply.github.com>
2022-12-29 20:03:24 +0530

    nk changes

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,4 @@
 # build_files.sh
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-#python3.9 manage.py makemigrations 
-#python3.9 manage.py migrate 
+p
